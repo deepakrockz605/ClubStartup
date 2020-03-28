4110438be8d66f0d2cba83a3375c5e08337b75c6
Siddharth Prajapati <siddharthp1995@gmail.com>
2020-03-28 16:34:26 +0530

    scroll spy added to header and components

--- src/App/Components/Header.js
+++ src/App/Components/Header.js
@@ -1,9 +1,8 @@
 import React, { PureComponent } from "react";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Signup from './Signup';
 import Login from './Login';
-// import GoogleAuth from './GoogleAuth';
 import '../CSS/Header.scss';
+import Scrollspy from 'react-scrollspy';
 
 class Header extends PureComponent {
     constructor(props) {
@@ -31,7 +30,7 @@ class Header extends PureComponent {
         const { isLogin } = this.state;
         return (
             <div className='Header-wrapper'>
-                <Tabs className='tabs'>
+                {/* <Tabs className='tabs'>
                     <TabList className='Header-tablist'>
                         <Tab className='Header-tab'>Home</Tab>
                         <Tab className='Header-tab'>About us</Tab>
@@ -51,14 +50,25 @@ class Header extends PureComponent {
                     <TabPanel className='TabPanel'>
                         <h2>Richie rich</h2>
                     </TabPanel>
-                </Tabs>
+                </Tabs> */}
+                <div className='tabs'>
+                    <Scrollspy
+                        className="Header-tablist" items={['section-1', 'section-2', 'section-3', 'section-4', 'section-5']}
+                        currentClassName="isCurrent">
+                        <li><a href="#section-1" className='Header-tab'>Home</a></li>
+                        <li><a href="#section-2" className='Header-tab'>About</a></li>
+                        <li><a href="#section-3" className='Header-tab'>Vision</a></li>
+                        <li><a href="#section-4" className='Header-tab'>Mission</a></li>
+                        <li><a href="#section-5" className='Header-tab'>Contact Us</a></li>
+                    </Scrollspy>
+                </div>
                 <div className='authentication'>
-                    <Signup/>
+                    <Signup />
                     {/* <Login/> */}
                     {/* <GoogleAuth /> */}
                     <div className='Login'>
-                        <button className='header-button' onClick = {this.updateState}>Log In</button>
-                        {isLogin && <Login callBackLogin = {this.callBackLogin}/>}
+                        <button className='header-button' onClick={this.updateState}>Log In</button>
+                        {isLogin && <Login callBackLogin={this.callBackLogin} />}
                     </div>
                 </div>
             </div>
@@ -66,4 +76,4 @@ class Header extends PureComponent {
     }
 }
 
-export default Header
+export default Header;
